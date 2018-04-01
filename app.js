b8e042cb30fca27a2a4992b6257f1e74e1030de4
Mridul Aggarwal <mridulaggarwal97@gmail.com>
2018-04-01 08:05:40 +0000

    add register routes

--- app.js
+++ app.js
@@ -15,6 +15,20 @@ app.set("view engine" , "ejs");
 app.use(express.static( __dirname + "/public"));
 seedDB();
 
+// Passport Configuration
+
+app.use(require("express-session")({
+    secret : "Rusty still wins the cutest dog competition",
+    resave : false,
+    saveUninitialised: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy (User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.get("/" , function(req , res){
    res.render("landing"); 
 });
@@ -98,6 +112,28 @@ app.post("/campgrounds/:id/comments", function(req,res){
              }
           });
       }
+   });
+});
+
+//==============
+// AUTH ROUTES
+//==============
+// show register form
+app.get("/register" , function(req ,res){
+   res.render("register"); 
+});
+
+// register logic
+app.post("/register" , function(req,res){
+   var newUser = new User({username : req.body.username});
+   User.register(newUser , req.body.password , function(err , user){
+       if(err){
+           console.log(err);
+           return res.redirect("/register");
+       }
+       passport.authenticate("local")(req , res , function(){
+            res.redirect("/"); 
+       });
    });
 });
 
