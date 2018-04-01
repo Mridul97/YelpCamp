1fa8f2aa1d5694dc96dde5a9e8efaa2b47e00d36
Mridul Aggarwal <mridulaggarwal97@gmail.com>
2018-04-01 20:07:09 +0000

    add login routes

--- app.js
+++ app.js
@@ -132,10 +132,24 @@ app.post("/register" , function(req,res){
            return res.redirect("/register");
        }
        passport.authenticate("local")(req , res , function(){
-            res.redirect("/"); 
+            res.redirect("/campgrouns"); 
        });
    });
 });
+
+// show login form
+app.get("/login" , function(req, res){
+    res.render("login");
+});
+
+// Login logic
+// app.post("/login" , middleware , callback);
+app.post("/login" ,passport.authenticate("local" , 
+    {
+        successRedirect : "/campgrounds",
+        failureRedirect : "/login"
+    }
+), function(req, res){});
 
 app.listen(process.env.PORT , process.env.IP, function(){
     console.log("Yelp Camp app is listening..");
